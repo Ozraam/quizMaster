df5c91aa17a5c4f20b40eaa8d6b8d14e37cd85f8
Tristan <lhgrillepain@gmail.com>
2023-09-24 00:06:42 +0200

    ✨ added UI for signup

--- public/quizMaster/login/script.js
+++ public/quizMaster/login/script.js
@@ -17,3 +17,51 @@ document.querySelector('.login-form').addEventListener('submit', async (e) => {
         window.location.replace('/dashboard');
     }
 });
+
+function switchForm() {
+    const login = document.querySelector('.login-form');
+    const signup = document.querySelector('.signup-form');
+    const container = document.querySelector('.container');
+
+    container.animate([
+        { filter: 'blur(0px)' },
+        { filter: 'blur(15px)' },
+    ], {
+        duration: 250,
+        forwards: true,
+    }).addEventListener('finish', () => {
+        login.classList.toggle('hide');
+        signup.classList.toggle('hide');
+        container.animate([
+                { filter: 'blur(15px)' },
+                { filter: 'blur(0px)' },
+            ],
+            {
+                duration: 250,
+                forwards: true,
+            })
+    })
+}
+
+document.querySelector('.switch-btn').addEventListener('click', () => switchForm())
+
+document.querySelector('.signup-form').addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const username = document.querySelector('#username-sign').value;
+    const password = document.querySelector('#password-sign').value;
+    const data = { username, password };
+    const response = await fetch('/API/signup', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data),
+    });
+    // status code 409 = username already exists
+    console.log(response.status);
+    if (response.status === 409) {
+        document.querySelector('.error').innerHTML = 'Username already exists';
+    } else if (response.status === 201) {
+        switchForm();
+        // display success message
+        document.querySelector('.error').innerHTML = 'Account created successfully';
+    }
+});
